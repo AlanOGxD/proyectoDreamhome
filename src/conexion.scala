053347061f36e0dc9d4a8260c622149d7b32335c
AlanOGxD <alanosvaldo88@gmail.com>
2021-05-20 22:05:19 -0500

    conexión reacomodada

--- src/conexion.scala
+++ src/conexion.scala
@@ -4,7 +4,7 @@ import java.sql.{Connection,DriverManager}
 
 object conexion extends App {
    
-   private var connection:Connection = null
+   var connection:Connection = null
 
   private def conexion(): Unit = {
     // connect to the database named "mysql" on port 8889 of localhost
@@ -12,11 +12,10 @@ object conexion extends App {
     val driver = "com.mysql.cj.jdbc.Driver"
     val username = "root"
     val password = "alanxD"
-    var connection:Connection = null
     try {
         Class.forName(driver)
         connection = DriverManager.getConnection(url, username, password)
-             val statement = connection.createStatement
+            /* val statement = connection.createStatement
         val rs = statement.executeQuery("Select * from branch")
         while(rs.next){
           val branch=rs.getString("branchNo")
@@ -24,18 +23,23 @@ object conexion extends App {
           val city = rs.getString("City")
          val postCode = rs.getString("PostCode")
           println("branch = %s, Street = %s, City = %s, Post= %s".format(branch, street, city, postCode))
-        }
+        }*/
         println("Conexion realizada")
     } catch {
         case e: Exception => e.printStackTrace
     }
-    connection.close
+    
    }
    
    def getConexion():Connection={
+     try{
+       print(connection)
   if(connection==null) conexion()
   
-    return connection
+     }catch{
+       case e: Exception => e.printStackTrace
+     }
+     return connection:Connection
 }
    
    
