31d9bcd5eb80012d36ef40b9dd8e8011af0e3801
AlanOGxD <alanosvaldo88@gmail.com>
2021-05-23 14:19:31 -0500

    Diseño login terminado

--- src/Login.scala
+++ src/Login.scala
@@ -1,6 +1,10 @@
 import scala.swing._
 import java.awt.Color
-
+import javax.swing.colorchooser.CenterLayout
+import javax.swing.ImageIcon
+import java.awt.image.BufferedImage
+import java.io.File                                                           
+import javax.imageio.ImageIO  
 
 object Login extends SimpleSwingApplication{
    
@@ -17,11 +21,19 @@ object Login extends SimpleSwingApplication{
     }
    
    val textPass = new TextField(""){
-     size_=(new Dimension(50, 200))
+     size_=(new Dimension(50, 50))
    }
-     contents = new GridPanel(7, 2){
+   contents= new BoxPanel(Orientation.Vertical){
+     size_=(new Dimension(800, 800))
+     contents += new Label("Inicio de sesion")
+     //val imagen=toolkit.getImage("/Imagenes/logo.jpg")
+     contents += new ImagePanel{
+       imagePath = ("Imagenes/logo.png")
+       size_=(new Dimension(100,100))
+         }
+       val grid = new GridPanel(7, 2){
         contents +=new Label("						")
-       contents +=new Label("Inicio de sesion")
+       contents +=new Label("						")
        contents += label
        contents += textUsuario 
        contents +=new Label("						")
@@ -41,12 +53,42 @@ object Login extends SimpleSwingApplication{
        contents +=new Label("						")
        contents +=new Label("						")
    }
+       contents += grid  
+       grid.hGap_=(2)
+       grid.vGap_=(3)
+       }
+     
     
     centerOnScreen()  
 } 
-   top.size_=(new Dimension(300,400))
+   top.size_=(new Dimension(300,550))
    
-    
-    
-   
+
+
+
 }
+
+class ImagePanel extends Panel                                                
+{                                                                             
+  private var _imagePath = ""                                                 
+  private var bufferedImage:BufferedImage = null                              
+
+  def imagePath = _imagePath                                                  
+
+  def imagePath_=(value:String)                                               
+  {                                                                           
+    _imagePath = value                                                        
+    bufferedImage = ImageIO.read(new File(_imagePath))                        
+  }                                                                           
+
+
+  override def paintComponent(g:Graphics2D) =                                 
+  {                                                                           
+    if (null != bufferedImage) g.drawImage(bufferedImage, 0, 0, null)         
+  }                                                                           
+}                                                                             
+
+object ImagePanel                                                             
+{                                                                             
+  def apply() = new ImagePanel()                                              
+} 
