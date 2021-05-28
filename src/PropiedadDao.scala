11a020dcffb425812673984c8ae75b5c8d8bbcc1
AlanOGxD <alanosvaldo88@gmail.com>
2021-05-28 03:58:41 -0500

    metodo busqueda por tabla

--- src/PropiedadDao.scala
+++ src/PropiedadDao.scala
@@ -88,6 +88,47 @@ object PropiedadDao{
      return branch
   }
   
+  def buscarPro(nopro:String):ListBuffer[Propiedades]={
+    var propiedadesx = new ListBuffer[Propiedades]()
+    val conn:Connection = conexion.getConexion()
+    val statement = conn.createStatement
+    val rs = statement.executeQuery("Select * from propertyforrent where PropertyNo = '"+nopro+"'")
+    while(rs.next){
+      val NoProperty=rs.getString("PropertyNo")
+      val street = rs.getString("street")
+      val city = rs.getString("city")
+      val postcode = rs.getString("postcode")
+      val types = rs.getString("type")
+      val rooms = rs.getByte("rooms")
+      val rent = rs.getInt("rent")
+      val noOwner = rs.getString("FK_ownerNo")
+      val noStaff = rs.getString("FK_staffNo")
+      val nobranch = rs.getString("FK_branchNo")
+      
+      val statement2 = conn.createStatement
+      val rs2 = statement2.executeQuery("Select * from privateowner where OwnerNo = '"+noOwner+"'")
+      var propietario=""
+     
+      while(rs2.next) {propietario=rs2.getString("fName")}
+      
+      
+      val statement3 = conn.createStatement
+      val rs3 = statement3.executeQuery("Select * from staff where StaffNo='"+noStaff+"'")
+      var personal=""
+      while(rs3.next) {personal=rs3.getString("fName")}
+            
+      val statement4 = conn.createStatement
+      val rs4 = statement4.executeQuery("Select * from branch where branchNo='"+nobranch+"'")
+      var sucursal=""
+     while(rs4.next) {sucursal = rs4.getString("Street")}
+      
+      
+      val pr = new Propiedades(NoProperty, street, city, postcode, types, rooms, rent, propietario, personal, sucursal)
+      propiedadesx.+=(pr)
+    }
+    return propiedadesx
+  }
+  
   def altas(P:String , S:String, C:String, PC:String, T:String, RS:Integer, R:Integer, PN:String, SN:String, BN:String):Unit={
     val st = conn.createStatement
     val sql = "Select * from privateowner where fName = '"+PN+"'"
